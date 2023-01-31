1e0e1551dca4643a3e81ce240d0704fdd0f6c364
ItchelZuniga <itchelzng@gmail.com>
2023-01-30 21:04:40 -0600

    Revert "Merge branch 'main' of https://github.com/ItchelZuniga/API-Rick-y-Morty"
    This reverts commit a13a344cf506464c5a08ec6a8bfadaa9cad02c57, reversing
    changes made to 76707e94b3925c04b797ebf858f8096391847b04.

--- fan_page/src/App.js
+++ fan_page/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
-import { useAuth0 } from "@auth0/auth0-react";
-
 import Login from './components/Login';
 import Logout from './components/Logout';
-import Profile from "./components/Profile";
+import Profile from './components/Profile';
 
 
 function App() {
-  const {isAuthenticated}=useAuth0();
   return (
-    <div className="App">
+    <div className='background'>
       <header className="App-header">
       <h1>Bienvenidos a la fan page de</h1>
       <p>Rick & Morty</p>
--- fan_page/src/components/Profile.jsx
+++ fan_page/src/components/Profile.jsx
@@ -1,12 +1,5 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import Home from "../pages/Home";
-import Episodios from "../pages/Episodios";
-import Lugares from "../pages/Lugares";
-import MiCuenta from "../pages/Micuenta";
-import Desarrolladoras from "../pages/Desarrolladoras";
 
 function Profile(){
     const { user, isAuthenticated, isLoading } = useAuth0();
@@ -18,23 +11,7 @@ function Profile(){
     return (
         isAuthenticated && (
         <div>
-            
-        
-
-        <div className="container">
-            <BrowserRouter>
-            <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/episodios" element={<Episodios />} />
-            <Route path="/lugares" element={<Lugares />} />
-            <Route path="/miCuenta" element={<MiCuenta />} />
-            <Route path="/desarrolladoras" element={<Desarrolladoras />} />
-          </Routes>
-          </BrowserRouter>
-
-        </div>
-            <img className="usser-image" src={user.picture} alt={user.name} />
+            <img src={user.picture} alt={user.name} />
             <h2>{user.name}</h2>
             <p>{user.email}</p>
         </div>
